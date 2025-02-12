161598dca5de0b40402c5e4731dcef0ff80178b9
James Wainwright <james.wainwright@lowrisc.org>
2025-02-12 17:34:11 +0000

    Add protocol for reading

--- sw/device/examples/bridge/main.c
+++ sw/device/examples/bridge/main.c
@@ -16,14 +16,6 @@ void configure_spi_device(dif_spi_device_handle_t *spi_device) {
     .tx_order = kDifSpiDeviceBitOrderMsbToLsb,
     .rx_order = kDifSpiDeviceBitOrderMsbToLsb,
     .device_mode = kDifSpiDeviceModeFlashEmulation,
-    // .device_mode = kDifSpiDeviceModeGeneric,
-    // .mode_cfg = {
-    //   .generic = {
-    //     .rx_fifo_commit_wait = 63,
-    //     .rx_fifo_len = 8,
-    //     .tx_fifo_len = 8,
-    //   },
-    // },
   };
   CHECK_DIF_OK(dif_spi_device_configure(spi_device, spi_config));
 
@@ -35,13 +27,21 @@ void configure_spi_device(dif_spi_device_handle_t *spi_device) {
     .payload_dir_to_host = true,
   };
 
-  dif_spi_device_flash_command_t read_cmd = {
+  dif_spi_device_flash_command_t read_4b_cmd = {
+    .opcode = kSpiDeviceFlashOpRead4b,
+    .address_type = kDifSpiDeviceFlashAddrCfg,
+    .dummy_cycles = 0,
+    .payload_io_type = kDifSpiDevicePayloadIoSingle,
+    .upload = true,
+    .set_busy_status = true,
+  };
+
+  dif_spi_device_flash_command_t read_normal_cmd = {
     .opcode = kSpiDeviceFlashOpReadNormal,
     .address_type = kDifSpiDeviceFlashAddrCfg,
     .dummy_cycles = 0,
     .payload_io_type = kDifSpiDevicePayloadIoSingle,
     .payload_dir_to_host = true,
-    .upload = true,
   };
 
   dif_spi_device_flash_command_t write_cmd = {
@@ -55,8 +55,9 @@ void configure_spi_device(dif_spi_device_handle_t *spi_device) {
   };
 
   CHECK_DIF_OK(dif_spi_device_set_flash_command_slot(spi_device, kSpiDeviceReadCommandSlotBase, kDifToggleEnabled, read_status1_cmd));
-  CHECK_DIF_OK(dif_spi_device_set_flash_command_slot(spi_device, kSpiDeviceWriteCommandSlotBase + 1, kDifToggleEnabled, read_cmd));
+  CHECK_DIF_OK(dif_spi_device_set_flash_command_slot(spi_device, kSpiDeviceReadCommandSlotBase + 5, kDifToggleEnabled, read_normal_cmd));
   CHECK_DIF_OK(dif_spi_device_set_flash_command_slot(spi_device, kSpiDeviceWriteCommandSlotBase, kDifToggleEnabled, write_cmd));
+  CHECK_DIF_OK(dif_spi_device_set_flash_command_slot(spi_device, kSpiDeviceWriteCommandSlotBase + 1, kDifToggleEnabled, read_4b_cmd));
 
   CHECK_DIF_OK(dif_spi_device_configure_flash_wren_command(spi_device, kDifToggleEnabled, kSpiDeviceFlashOpWriteEnable));
   CHECK_DIF_OK(dif_spi_device_configure_flash_en4b_command(spi_device, kDifToggleEnabled, kSpiDeviceFlashOpEnter4bAddr));
@@ -68,7 +69,7 @@ void bare_metal_main(void) {
 
   dbg_printf("CONFIGURED\r\n");
 
-  uint8_t magic = 'X';
+  uint32_t magic = 0xdeadbeef;
   dbg_printf("magic addr 0x%p\r\n", &magic);
 
   while (true) {
@@ -79,46 +80,44 @@ void bare_metal_main(void) {
       dbg_printf("cmd had no address\r\n");
       break;
     }
-    mmio_region_t base_addr = mmio_region_from_addr(upload_info.address);
 
-    uint8_t data;
-    uint16_t occupancy;
-    uint32_t fifo_start;
+    // Variables cannot be declared within a switch:
+    mmio_region_t base_addr;
+    // 8-bit and 32-bit data read from OpenTitan memory:
+    uint8_t data8;
+    uint32_t data32;
 
     switch (upload_info.opcode) {
-      case kSpiDeviceFlashOpReadNormal:
-        CHECK_DIF_OK(dif_spi_device_get_flash_payload_fifo_occupancy(&spi_device, &occupancy, &fifo_start));
-        dbg_printf("before occupancy %d, %d\r\n", occupancy, fifo_start);
+      case kSpiDeviceFlashOpRead4b:
+        base_addr = mmio_region_from_addr(upload_info.address);
 
-        data = mmio_region_read8(base_addr, 0);
-        CHECK_DIF_OK(
-          dif_spi_device_write_flash_buffer(&spi_device, kDifSpiDeviceFlashBufferTypeEFlash, 0, 1, &data)
-        );
+        if (upload_info.data_len == 0) {
+          data8 = mmio_region_read8(base_addr, 0);
+          CHECK_DIF_OK(dif_spi_device_write_flash_buffer(&spi_device, kDifSpiDeviceFlashBufferTypeEFlash, 0, 1, &data8));
+          break;
+        }
 
-        CHECK_DIF_OK(dif_spi_device_get_flash_payload_fifo_occupancy(&spi_device, &occupancy, &fifo_start));
-        dbg_printf("after occupancy %d, %d\r\n", occupancy, fifo_start);
+        for (uint16_t i = 0; i < upload_info.data[0] i += 4) {
+          data32 = mmio_region_read32(base_addr, i);
+          CHECK_DIF_OK(dif_spi_device_write_flash_buffer(&spi_device, kDifSpiDeviceFlashBufferTypeEFlash, i, sizeof(data32), (uint8_t*)&data32));
+        }
+
         break;
       case kSpiDeviceFlashOpPageProgram:
+        base_addr = mmio_region_from_addr(upload_info.address);
         if (upload_info.data_len == 0) {
           dbg_printf("program cmd to addr %p had no data\r\n", base_addr.base);
         } else if (upload_info.data_len == 1) {
-          mmio_region_write8(base_addr, 0, upload_info.data[0]);
+            mmio_region_write8(base_addr, 0, upload_info.data[0]);
         } else {
           dbg_printf("TODO: multi-byte program\r\n");
         }
         break;
       default:
-        dbg_printf("unknown opcode: %d\r\n", upload_info.opcode);
+        dbg_printf("unknown opcode: %x\r\n", upload_info.opcode);
     }
 
     CHECK_DIF_OK(dif_spi_device_clear_flash_busy_bit(&spi_device));
-    
-    // uint8_t buf[1];
-    // size_t bytes_received = 0;
-    // CHECK_DIF_OK(dif_spi_device_recv(&spi_device, buf, sizeof(buf), &bytes_received));
-    // if (bytes_received > 0) {
-    //   dbg_printf("received: %x\r\n", buf[0]);
-    // }
   }
 }
 
